8d9d33c5114850468bbd995aecef35af301d4952
Dmitriy Volchenko <dima201246@gmail.com>
2016-12-09 13:12:18 +0300

    Restructuresed

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@
 using namespace sf;
 using namespace std;
 
+#define MAX_PLAYERS	2
+#define MAX_EMINEMS	20
+
 #define GAME_SPEED	900
 
 #define UP_SIDE		0
@@ -32,9 +35,11 @@ string first_map[13]	= {
 "     wews    "
 };
 
+class tank;
 
 class bullet {
-	unsigned char	side;
+	int	side,
+					other_tanks_num;
 
 	bool			status;
 
@@ -44,34 +49,13 @@ class bullet {
 	SoundBuffer		buffer_shot,
 					buffer_un_shot;
 
+	tank			*other_tanks;
+
 	FloatRect		rect;
 	Sound 			*sound_shot,
 					*sound_un_shot;
 
-	void collision() {
-		if (dx != 0) {
-			if (first_map[int((rect.top + 3) / 16)][int(rect.left / 16)] == 'w') {
-				first_map[int((rect.top + 3) / 16)][int(rect.left / 16)]	= ' ';
-				dx	= 0;
-			}
-
-			if (first_map[int((rect.top + 3) / 16)][int(rect.left / 16)] == 'a') {
-				dx	= 0;
-			}
-		}
-
-		if (dy != 0) {
-			if (first_map[int(rect.top / 16)][int((rect.left + 3) / 16)] == 'w') {
-				first_map[int(rect.top / 16)][int((rect.left + 3) / 16)]	= ' ';
-				dy	= 0;
-			}
-
-			if (first_map[int(rect.top / 16)][int((rect.left + 3) / 16)] == 'a') {
-				dy	= 0;
-			}
-		}
-	}
-
+	void collision();
 public:
 
 	Sprite			sprite;
@@ -93,8 +77,10 @@ public:
 		delete sound_un_shot;
 	}
 
-	void init(Texture &image) {
+	void init(Texture &image, tank *g_other_tanks, int g_other_tanks_num) {
 		sprite.setTexture(image);
+		other_tanks		= g_other_tanks;
+		other_tanks_num	= g_other_tanks_num;
 	}
 
 	void update(float time) {
@@ -124,9 +110,11 @@ public:
 			rect.top	+= dy * (time * 1.5);
 			sprite.setPosition(rect.left, rect.top);
 		}
+
+
 	}
 
-	void shot(int posX, int posY, unsigned char side) {
+	void shot(int posX, int posY, int side) {
 		sound_shot->play();
 		sprite.setTextureRect(IntRect(320 + ((side / 2) * 8), 100, 8, 8));
 		if (side == 0) {
@@ -155,114 +143,29 @@ public:
 	}
 };
 
-class eminem {
-
-	unsigned char	life,
-					level,
-					left_tank,
-					currentFrame,
-					side;
-					
-	float			dx,
-					dy;
-
-	FloatRect		rect;
-
-
-public:
-
-	bullet			shell;
-	Sprite			sprite;
-
-	eminem(Texture &image) {
-		sprite.setTexture(image);
-		shell.init(image);
-		side			= 0;
-		life			= 3;
-		level			= 1;
-		rect			= FloatRect(0, 0, 16, 16);
-		left_tank		= 16;
-		currentFrame	= 0;
-		dx	= dy		= 0.0;
-		rect.left		= 144;
-		rect.top		= 200;
-	}
-
-	void update(float time) {
-		if ((dx	!= 0) || (dy != 0)) {	// Защита от выхода за границы
-			if ((dx > 0) && (((rect.left + rect.width) / 16) > maxX)) dx	= 0;
-			if ((dy > 0) && (((rect.top + rect.height) / 16) > (maxY - 1))) dy	= 0;
-			if ((dx < 0) && (rect.left <= 1)) dx	= 0;
-			if ((dy < 0) && (rect.top <= 1)) dy	= 0;
-		}
-
-		rect.left		+= dx * (time / 2.0);	// Собсно, движение
-		rect.top 		+= dy * (time / 2.0);
-		// sprite.setTextureRect(IntRect(320, 72, 16, 16));
-
-		sprite.setTextureRect(IntRect(128 + (side * 16), 0, 16, 16));
-
-		if ((dx	!= 0) || (dy != 0)) {
-			currentFrame	!= currentFrame;
-
-			if (dy < 0)
-				side	= 0 + currentFrame;
-
-			if (dx < 0)
-				side	= 2 + currentFrame;
-
-			if (dy > 0)
-				side	= 4 + currentFrame;
-			
-			if (dx > 0)
-				side	= 6 + currentFrame;
-
-		}
-
-		sprite.setPosition(rect.left, rect.top);
-
-		dx=0;
-		dy=0;
-	}
-
-	void piu_piu() {
-		if (!shell.active()) {
-			shell.shot(rect.left, rect.top, side);
-		}
-	}
-
-	void move_up() {
-		dy	-= 0.1;
-	}
-
-	void move_down() {
-		dy	+= 0.1;
-	}
-
-	void move_left() {
-		dx	-= 0.1;
-	}
-
-	void move_right() {
-		dx	+= 0.1;
-	}
-};
-
-
-class player {
-	unsigned char	life,
+class tank {
+	int				life,
 					level,
 					left_tank,
 					currentFrame,
 					num_of_player,
-					side;
+					side,
+					other_tanks_num;
 
 	float			dx,
-					dy;
+					dy,
+					l_time;
+
+	tank			*other_tanks;
 
 	FloatRect		rect;
 
-	bool			god_mode;
+	bool			god_mode,
+					ii;
+
+	Sprite			t_sprite;
+
+	bullet			shell;
 
 	void collision() {
 		char	block_1, block_2;
@@ -293,15 +196,18 @@ class player {
 
 public:
 
-	bullet			shell;
-	Sprite			sprite;
+	tank(){}
+	~tank(){}
 
-	player(Texture &image, unsigned char num) {
-		sprite.setTexture(image);
-		shell.init(image);
+	void init(Texture &image, int num, bool g_ii, int g_life, tank *g_other_tanks, int g_other_tanks_num) {
+		t_sprite.setTexture(image);
+		shell.init(image, g_other_tanks, g_other_tanks_num);
 		side			= 0;
-		life			= 3;
+		life			= g_life;
 		level			= 1;
+		other_tanks		= g_other_tanks;
+		other_tanks_num	= g_other_tanks_num;
+		ii				= g_ii;
 		num_of_player	= num;
 		rect			= FloatRect(0, 0, 16, 16);
 		god_mode		= false;
@@ -313,6 +219,8 @@ public:
 	}
 
 	void update(float time) {
+		l_time	=time;
+
 		if ((dx	!= 0) || (dy != 0)) {	// Защита от выхода за границы
 			if ((dx > 0) && (((rect.left + rect.width) / 16) > maxX)) dx	= 0;
 			if ((dy > 0) && (((rect.top + rect.height) / 16) > (maxY - 1))) dy	= 0;
@@ -325,7 +233,7 @@ public:
 		rect.left		+= dx * (time / 2.0);	// Собсно, движение
 		rect.top 		+= dy * (time / 2.0);
 
-		sprite.setTextureRect(IntRect(0 + (side * 16), 0, 16, 16));
+		t_sprite.setTextureRect(IntRect(0 + (side * 16), 0, 16, 16));
 
 		if ((dx	!= 0) || (dy != 0)) {
 			currentFrame	!= currentFrame;
@@ -344,7 +252,7 @@ public:
 
 		}
 
-		sprite.setPosition(rect.left, rect.top);
+		t_sprite.setPosition(rect.left, rect.top);
 
 		dx=0;
 		dy=0;
@@ -372,17 +280,71 @@ public:
 		dx	+= 0.1;
 	}
 
-	unsigned char getSide() {
+	int getSide() {
 		return side;
 	}
+
+	FloatRect getRect() {
+		return rect;
+	}
+
+	int getLeft_tank() {
+		return left_tank;
+	}
+
+	int getLife() {
+		return life;
+	}
+
+	void draw(RenderWindow &window) {
+		if (life == 0)
+			return;
+
+		window.draw(t_sprite);
+
+		if (shell.active()) {
+			shell.update(l_time);
+			window.draw(shell.sprite);
+		}
+	}
 };
+
+void bullet::collision() {
+		if (dx != 0) {
+			if (first_map[int((rect.top + 3) / 16)][int(rect.left / 16)] == 'w') {
+				first_map[int((rect.top + 3) / 16)][int(rect.left / 16)]	= ' ';
+				dx	= 0;
+			}
+
+			if (first_map[int((rect.top + 3) / 16)][int(rect.left / 16)] == 'a') {
+				dx	= 0;
+			}
+		}
+
+		if (dy != 0) {
+			if (first_map[int(rect.top / 16)][int((rect.left + 3) / 16)] == 'w') {
+				first_map[int(rect.top / 16)][int((rect.left + 3) / 16)]	= ' ';
+				dy	= 0;
+			}
+
+			if (first_map[int(rect.top / 16)][int((rect.left + 3) / 16)] == 'a') {
+				dy	= 0;
+			}
+		}
+
+		if (dx != dy) {
+			for (int	i	= 0; i < other_tanks_num; ++i) {
+				if (rect.intersects(other_tanks[i].getRect())) {	// Проверка, попала ли пуля в кого-нибудь
+
+				}
+			}
+		}
+	}
 
 int main() {
 	Clock			clock;										// Переменная, для слежения за временем
 
 	Texture 		texture;
-
-	RectangleShape	rectangle(Vector2f(32,32));
 
 	Event 			event;
 
@@ -393,25 +355,34 @@ int main() {
 					k_u,
 					k_d;
 
-	RenderWindow window( VideoMode(512, 480), "Bottle city");	// Создание окна
-	texture.loadFromFile("media/textures.png");						// Загрузка всех текстур
+	tank			players[MAX_PLAYERS],						// Создание игроков
+					eminems[MAX_EMINEMS];
 
-	Sprite			sprite(texture);
-	player			p1(texture, 1);
-	eminem			p2(texture);
+	RenderWindow window(VideoMode(512, 480), "Bottle city");	// Создание окна
+	texture.loadFromFile("media/textures.png");					// Загрузка всех текстур
+
+	Sprite			sprite(texture);							// Загрузка спрайта
+
+	players[0].init(texture, 1, false, 2, eminems, MAX_EMINEMS);
+	players[1].init(texture, 2, false, 0, eminems, MAX_EMINEMS);
+
+	for (int	i = 0; i < MAX_EMINEMS; ++i) {
+		eminems[i].init(texture, 0, true, 1, players, MAX_PLAYERS);
+	}
 
 	while (window.isOpen()) {
 		time = clock.getElapsedTime().asMicroseconds();			// Получение времени
 		clock.restart();										// Сброс часиков
 
-		time = time / GAME_SPEED;
+		time = time / GAME_SPEED;								// Установка скорости игры
 
-		if (time > 20)	
+		if (time > 20)
 			time = 20;
 
-		while (window.pollEvent(event)) {
-			if (event.type == Event::Closed)
+		while (window.pollEvent(event)) {						// Отслеживание события закрытия окна
+			if (event.type == Event::Closed) {
 				window.close();
+			}
 		}
 
 		/*Защита от диагоналей Начало*/
@@ -421,50 +392,50 @@ int main() {
 		k_l	= true;
 
 		if ((Keyboard::isKeyPressed(Keyboard::Left)) && (Keyboard::isKeyPressed(Keyboard::Down))) {
-			if (p1.getSide() == LEFT_SIDE)	k_d	= false;
+			if (players[0].getSide() == LEFT_SIDE)	k_d	= false;
 			else k_l	= false;
 		}
 
 		if ((Keyboard::isKeyPressed(Keyboard::Left)) && (Keyboard::isKeyPressed(Keyboard::Up)))	{
-			if (p1.getSide() == LEFT_SIDE)	k_u	= false;
+			if (players[0].getSide() == LEFT_SIDE)	k_u	= false;
 			else k_l	= false;
 		}
 
 		if ((Keyboard::isKeyPressed(Keyboard::Right)) && (Keyboard::isKeyPressed(Keyboard::Down))) {
-			if (p1.getSide() == RIGHT_SIDE)	k_d	= false;
+			if (players[0].getSide() == RIGHT_SIDE)	k_d	= false;
 			else k_r	= false;
 		}
 
 		if ((Keyboard::isKeyPressed(Keyboard::Right)) && (Keyboard::isKeyPressed(Keyboard::Up))) {
-			if (p1.getSide() == RIGHT_SIDE)	k_u	= false;
+			if (players[0].getSide() == RIGHT_SIDE)	k_u	= false;
 			else k_r	= false;
 		}
 		/*Защита от диагоналей Конец*/
 
 		/*Обработка кнопок Начало*/
 		if ((Keyboard::isKeyPressed(Keyboard::Left)) && (k_l)) {
-			p1.move_left();
+			players[0].move_left();
 		}
 
 		if ((Keyboard::isKeyPressed(Keyboard::Right)) && (k_r)) {
-			p1.move_right();
+			players[0].move_right();
 		}
 
 		if ((Keyboard::isKeyPressed(Keyboard::Up)) && (k_u)) {
-			p1.move_up();
+			players[0].move_up();
 		}
 
 		if ((Keyboard::isKeyPressed(Keyboard::Down)) && (k_d)) {
-			p1.move_down();
+			players[0].move_down();
 		}
 
 		if (Keyboard::isKeyPressed(Keyboard::Z)) {
-			p1.piu_piu();
+			players[0].piu_piu();
 		}
 		/*Обработка кнопок Конец*/
 
-		p1.update(time);
-		p2.update(time);
+		players[0].update(time);
+		// p2.update(time);
 
 		window.clear(Color(0,0,0));
 		for (int	i	= 0; i < maxX; ++i) {
@@ -493,13 +464,9 @@ int main() {
 		sprite.setPosition(208, 0) ; 
 		window.draw(sprite);
 
-		if (p1.shell.active()) {
-			p1.shell.update(time);
-			window.draw(p1.shell.sprite);
+		for (int	i	= 0; i < MAX_PLAYERS; ++i) {			// Отрисовка игроков
+			players[i].draw(window);
 		}
-
-		window.draw(p1.sprite);
-		window.draw(p2.sprite);
 
 		for (int	i	= 0; i < maxX; ++i) {
 			for (int	j	= 0; j < maxY; ++j) {
