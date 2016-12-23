c918266adf34effe15bd1bd15c67293635d3ab6d
dima201246 <dima201246@gmail.com>
2016-12-23 23:32:39 +0300

    Fix

--- src/g_pause.cpp
+++ src/g_pause.cpp
@@ -1,6 +1,7 @@
 #include "../include/obj.hpp"
 
-GPause::GPause(sf::Texture &image, GameMap *main_map) {
+GPause::GPause(sf::Texture &image, GameMap *main_map)
+{
 	sprite_.setTexture(image);
 	sprite_.scale(SCALE_X, SCALE_Y);
 	sprite_.setTextureRect(sf::IntRect(288, 175, 40, 8));
@@ -8,16 +9,21 @@ GPause::GPause(sf::Texture &image, GameMap *main_map) {
 	statusGame_	= false;
 }
 
-void GPause::paused(sf::RenderWindow &window) {
-	if (!statusGame_) {
+void GPause::paused(sf::RenderWindow &window)
+{
+	if (!statusGame_)
+	{
 		window.draw(sprite_);
         window.display();
 		statusGame_	= true;
-	} else {
+	}
+	else
+	{
 		statusGame_	= false;
 	}
 }
 
-bool GPause::status() {
+bool GPause::status()
+{
 	return statusGame_;
 }
